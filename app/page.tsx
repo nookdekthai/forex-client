be0aa4b3f16642596d61a31d5e199dfb3b1aaed7
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-10-17 22:21:33 +0700

    no message

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export const revalidate = 3600
 
 // http://localhost:8000/api/v1/get-layout/Banner
 const Page = async () => {
+  console.log('porcess env =>',process.env.NEXT_PUBLIC_SERVER_URI);
+  
   const pmBanner = fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/get-layout/Banner`, {})
   const pmCategory = fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/get-layout/Categories`, {})
   const [resBanner, resCategory] = await Promise.all([pmBanner, pmCategory])
