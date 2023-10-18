75f2fc7c3c5cb8558ae88264c130cfeb337cd2eb
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-10-18 22:34:25 +0700

    no message

--- redux/features/api/apiSlice.ts
+++ redux/features/api/apiSlice.ts
@@ -3,8 +3,10 @@ import { userLoggedIn } from "../auth/authSlice";
 
 export const apiSlice = createApi({
   reducerPath: "api",
+  
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_SERVER_URI,
+    credentials: 'include'
   }),
   endpoints: (builder) => ({
     refreshToken: builder.query({
