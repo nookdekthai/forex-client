c16b6c31b19ead60a76050397a1586fc0e2a8f13
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-10-23 23:40:14 +0700

    no message

--- next.config.js
+++ next.config.js
@@ -18,10 +18,6 @@ const nextConfig = {
         source: '/api/v1/:path*',
         destination: `${process.env.SERVER_URI}/api/v1/:path*`,
       },
-      // {
-      //   source: '/me',
-      //   destination: `${process.env.SERVER_URI}/api/v1/me`,
-      // },
     ]
   },
 }
