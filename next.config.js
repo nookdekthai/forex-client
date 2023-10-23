122743fa6b56a82e961131440527e8143fbfff45
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-10-23 23:08:46 +0700

    Revert "Revert "Revert "no message"""
    This reverts commit 76f464fda66d0f2f016faad7691c37db71681541.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com','randomuser.me'],
+  images: {
+    domains: ['res.cloudinary.com', 'randomuser.me'],
+  },
+  experimental: {
+    reactRoot: true,
+    suppressHydrationWarning: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/social-auth',
+        destination: `${process.env.SERVER_URI}/api/v1/social-auth`,
       },
-      experimental:{
-        reactRoot: true,
-        suppressHydrationWarning: true,
+      {
+        source: '/me',
+        destination: `${process.env.SERVER_URI}/api/v1/me`,
       },
-      eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- redux/features/auth/authApi.ts
+++ redux/features/auth/authApi.ts
@@ -67,7 +67,7 @@ export const authApi = apiSlice.injectEndpoints({
     }),
     socialAuth: builder.mutation({
       query: ({ email, name, avatar }) => ({
-        url: "social-auth",
+        url: `${window.location.origin}/social-auth`,
         method: "POST",
         body: {
           email,
