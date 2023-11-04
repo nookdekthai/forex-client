809fbf4d5eb59e9b2d9eeacb9dc62ecbea793713
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-11-04 15:40:11 +0700

    no message

--- app/components/Ebook/pdf-worker.tsx
+++ app/components/Ebook/pdf-worker.tsx
@@ -1,6 +1,9 @@
-if (process.env.NODE_ENV === "production") {
-    // use minified verion for production
-    module.exports = require("pdfjs-dist/build/pdf.worker.min.js");
-} else {
-    module.exports = require("pdfjs-dist/build/pdf.worker.js");
-}
+import pdfWorkerMinProd from "pdfjs-dist/build/pdf.worker.min.js";
+// import pdfWorkerMinDev from "pdfjs-dist/build/pdf.worker.js";
+
+export default pdfWorkerMinProd
+// if (process.env.NODE_ENV === "production") {
+//     // use minified verion for production
+// } else {
+//     module.exports = require("pdfjs-dist/build/pdf.worker.js");
+// }
