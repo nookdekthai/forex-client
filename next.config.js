fd79e945f7c5debf5c942d7d0ed37803d15f9b9c
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-11-05 11:27:09 +0700

    no message

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   images: {
     domains: ['res.cloudinary.com', 'randomuser.me', 'cdn-local.mebmarket.com'],
   },
