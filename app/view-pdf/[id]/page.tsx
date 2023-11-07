dc05c8235792a4b1b21623914fb898642660e801
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-11-07 18:03:08 +0700

    no message

--- app/view-pdf/[id]/page.tsx
+++ app/view-pdf/[id]/page.tsx
@@ -9,9 +9,6 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-
-import PDFViewer from 'pdf-viewer-reactjs'
-
 // Import the styles provided by the react-pdf-viewer packages
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
