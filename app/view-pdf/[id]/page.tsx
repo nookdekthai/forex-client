c812810571ecd96484262b379c1f871448f2f052
Teerapong Sitthikarn <git-ssdd-user@truecorp.co.th>
2023-11-07 17:51:13 +0700

    add new pdf viewer

--- app/view-pdf/[id]/page.tsx
+++ app/view-pdf/[id]/page.tsx
@@ -3,12 +3,28 @@ import dynamic from 'next/dynamic';
 import React, { useState } from 'react'
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-const PDFViewer = dynamic(() => import("../../components/Ebook/PDFViewer"), {
-    ssr: false
-});
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+
+import PDFViewer from 'pdf-viewer-reactjs'
+
+// Import the styles provided by the react-pdf-viewer packages
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+// const PDFViewer = dynamic(() => import("../../components/Ebook/PDFViewer"), {
+//     ssr: false
+// });
+
+
+
 
 const page = ({ params }: any) => {
     const [open, setOpen] = useState(false);
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
     const [activeItem, setActiveItem] = useState(2);
     const [route, setRoute] = useState("Login");
 
@@ -21,7 +37,26 @@ const page = ({ params }: any) => {
                 setRoute={setRoute}
                 route={route}
             />
-            <div className='w-full text-center py-10'><PDFViewer link={`${process.env.NEXT_PUBLIC_ORIGIN_URI}/api/v1/get-ebook/${params.id}/download`} /></div>
+            <div className='w-full text-center py-10'>
+                {/* <PDFViewer link={`${process.env.NEXT_PUBLIC_ORIGIN_URI}/api/v1/get-ebook/${params.id}/download`} /> */}
+                {/* <PDFViewer
+                    withCredentials={true}
+                    document={{
+                        url: `${process.env.NEXT_PUBLIC_ORIGIN_URI}/api/v1/get-ebook/${params.id}/download`,
+                    }}
+                /> */}
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js">
+                    <div style={{ height: '750px' }}>
+                        <Viewer
+                            withCredentials={true}
+                            fileUrl={`${process.env.NEXT_PUBLIC_ORIGIN_URI}/api/v1/get-ebook/${params.id}/download`}
+                            plugins={[
+                                defaultLayoutPluginInstance,
+                            ]}
+                        />
+                    </div>
+                </Worker>
+            </div>
             <Footer />
 
         </>
